b875f88ef068f28ecdcebb7af3414babb7816fb6
Luca <luca@weve-got-it.com>
2022-08-24 11:58:16 +0200

    Initial Commit

---
+++ Project-8-Moonshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project-8-Moonshot
+//
+//  Created by Luca Capriati on 2022/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
